5286d07130762aa7cee22b65ab9772556d090b53
TOSHIKI-PC\Imagawa <comcreate.jp@gmail.com>
2017-11-26 01:25:38 +0900

    [update] sample speed test

---
+++ Assets/UniRedux.Sample/Singleton/Binary/ToDoApplication.cs
@@ -0,0 +1,13 @@
+﻿namespace UniRedux.Sample.Singleton.Binary
+{
+    public class ToDoApplication : Application<ToDoState, ToDoApplication>
+    {
+        protected override IStore<ToDoState> InitStore
+        {
+            get
+            {
+                return Redux.CreateDeepFreezeStore(ToDoReducer.Execute, ToDoReducer.InitState, UniReduxMiddleware.Logger, UniReduxMiddleware.CheckImmutableUpdate);
+            }
+        }
+    }
+}

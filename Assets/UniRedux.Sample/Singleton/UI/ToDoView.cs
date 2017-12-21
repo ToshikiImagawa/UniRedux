54449952382bbebad487f7f184f2d67cf5539d0e
ToshikiImagawa <shibainusamulai@gmail.com>
2017-12-21 14:44:44 +0900

    [update] Used RxBehaviour in sample code

--- Assets/UniRedux.Sample/Singleton/UI/ToDoView.cs
+++ Assets/UniRedux.Sample/Singleton/UI/ToDoView.cs
@@ -1,38 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+using UniRedux.EventSystems;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace UniRedux.Sample.Singleton.UI
 {
-    public abstract class ToDoView : UIBehaviour, IObserver<ToDo[]>
+    public abstract class ToDoView : RxUIBehaviour<ToDoState>
     {
         [SerializeField] public ToDoElement _toDoElementPrefab;
         private readonly IList<ToDoElement> _toDoElementObjectList = new List<ToDoElement>();
-        
+        private int[] _toDoIds = new int[0];
+
         private ScrollRect _scrollRect;
         private ScrollRect ScrollRect => _scrollRect ?? (_scrollRect = GetComponent<ScrollRect>());
 
-        public void OnCompleted()
+        private void HandleChange()
         {
-        }
+            var toDoIds = State.GetFilterToDos().Select(todo => todo.Id).ToArray();
+            if (toDoIds.SequenceEqual(_toDoIds)) return;
+            _toDoIds = toDoIds;
 
-        public void OnError(Exception error)
-        {
-            Debug.LogError(error);
-        }
-
-        public void OnNext(ToDo[] value)
-        {
-            var _toDoIds = value.Select(todo => todo.Id).ToArray();
             if (_toDoIds.Length < _toDoElementObjectList.Count)
             {
                 for (var i = _toDoElementObjectList.Count - 1; i >= _toDoIds.Length; i--)
                 {
                     var toDoElement = _toDoElementObjectList[i];
-                    toDoElement.Dispose();
                     toDoElement.gameObject.SetActive(false);
                 }
             }
@@ -51,11 +44,5 @@ namespace UniRedux.Sample.Singleton.UI
                 toDoElement.Init(toDoId);
             }
         }
-
-        protected override void Start()
-        {
-            ToDoSelector.FilterToDos(CurrentStore).Subscribe(this);
-        }
-        protected abstract IStore<ToDoState> CurrentStore { get; }
     }
 }
